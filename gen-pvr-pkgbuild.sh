341e4ccc78553a0baac970bb8154fd8d378f1c0f
BurningC4 <wangweichen1995@hotmail.com>
2022-07-24 00:12:57 +0800

    Update gen-pvr-pkgbuild.sh

--- gen-pvr-pkgbuild.sh
+++ gen-pvr-pkgbuild.sh
@@ -15,13 +15,16 @@ for REPONAME in $(cat temp/repos.json | jq -r .[].name | grep -v "pvr-scripts") 
   if [[ $GITVER ]];then
     if [[ $OLD_GITVER != $GITVER ]]; then
       PKGVER=$(curl -sL https://github.com/kodi-pvr/$REPONAME/raw/$KODI_RELEASE/$REPONAME/addon.xml.in | grep "\s\sversion=" | sed "s/.*=\"//g" | sed "s/\".*//g")
+      echo $PKGVER > temp/$PKGNAME/PKGVER
       echo "$PKGNAME has new version!"
       OLD_PKGVER=$(curl -sL https://github.com/BurningC4/kodi-pvr-pkgbuild/raw/release/$PKGNAME/PKGBUILD | grep "pkgver=" | sed "s/pkgver=//g")
       if [[ $PKGVER == $OLD_PKGVER ]]; then
         OLD_PKGREL=$(curl -sL https://github.com/BurningC4/kodi-pvr-pkgbuild/raw/release/$PKGNAME/PKGBUILD | grep "pkgrel=" | sed "s/pkgrel=//g")
         PKGREL=$(($OLD_PKGREL+1))
+        echo $PKGREL > temp/$PKGNAME/PKGREL
       else
         PKGREL=1
+        echo $PKGREL > temp/$PKGNAME/PKGREL
       fi
     else
       echo "$PKGNAME is up to date!"
@@ -34,7 +37,9 @@ for REPONAME in $(cat temp/repos.json | jq -r .[].name | grep -v "pvr-scripts") 
     echo "$REPONAME has no branch for KODI $KODI_RELEASE. Won't generate/upgrade and will be deleted."
     continue
   fi
-  echo $PKGNAME now update to $PKGVER-$PKGREL
+  PKGVER=$(cat temp/$PKGNAME/PKGVER)
+  PKGREL=$(cat temp/$PKGNAME/PKGREL)
+  echo $PKGNAME now $PKGVER-$PKGREL
   cat PKGBUILD.txt | sed "s/_PKGNAME_/$PKGNAME/g" | sed "s/_PKGVER_/$PKGVER/g" | sed "s/_PKGREL_/$PKGREL/g" | sed "s/_DESCRIPTION_/$DESCRIPTION/g" | sed "s/_REPONAME_/$REPONAME/g" | sed "s/_GITVER_/$GITVER/g" | sed "s/_SHA512_/$SHA512/g" > release/$PKGNAME/PKGBUILD
 done
 popd > /dev/null
